8b0d3f8d8a407e49778b8900b4d921036b1aec0e
Frederic Brodbeck <der.froedi@gmail.com>
2021-09-14 21:34:57 +0200

    label position / alignment

--- src/components.js
+++ src/components.js
@@ -1,4 +1,5 @@
-const { St } = require('gi')
+const { St, Graphene } = imports.gi
+const Clutter = imports.gi.Clutter
 
 var FOCUS_WINDOW_STYLE = 'extension-overview-navigation-window-tooltip'
 
@@ -8,7 +9,23 @@ var Label = class Label {
     this.gLabel = new St.Label({ })
     this.gLabel.set_style_class_name(FOCUS_WINDOW_STYLE)
 
+    // align with parent element
+    this.gLabel.add_constraint(new Clutter.AlignConstraint({
+      source: parent,
+      align_axis: Clutter.AlignAxis.X_AXIS,
+      factor: 0.5,
+    }));
+    this.gLabel.add_constraint(new Clutter.AlignConstraint({
+      source: parent,
+      align_axis: Clutter.AlignAxis.Y_AXIS,
+      // pivot_point: new Graphene.Point({ x: 0.5, y: 1.0 }),
+      factor: 1.0,
+    }));
     parent.add_child(this.gLabel)
+
+    // add offset
+    this.gLabel.translation_x = -50;
+    // this.gLabel.translation_y = -2;
   }
 
   updateFontColor (color) {
